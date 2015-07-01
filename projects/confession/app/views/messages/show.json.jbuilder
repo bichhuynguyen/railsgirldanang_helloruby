ef1729d8f0719743f7a58999383a00d38f58ac70
bichhuy_nguyen <bichhuy.nguyen.com>
2015-07-01 23:23:08 +0700

    Create confession app

---
+++ projects/confession/app/views/messages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @message, :id, :title, :content, :picture, :created_at, :updated_at
---
+++ projects/confession/db/migrate/20150629110953_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration
+  def change
+    create_table :messages do |t|
+      t.string :title
+      t.text :content
+      t.string :picture
+
+      t.timestamps null: false
+    end
+  end
+end
